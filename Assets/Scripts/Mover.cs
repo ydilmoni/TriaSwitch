a6a3a15d24863a0e1b8f3c1473fa455897e03f63
yehonatna <yonidil1997@gmail.com>
2024-12-03 07:49:15 +0200

    fix linter

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -6,7 +6,7 @@ public class Mover : MonoBehaviour
     [SerializeField]
     private float startRedAngle, endRedAngle, startGreenAngle, endGreenAngle, startBlueAngle, endBlueAngle;
 
-    private float currentAngle, jumpCounter=0;
+    private float currentAngle, jumpCounter = 0;
     [SerializeField]
     InputAction moveRight = new InputAction(type: InputActionType.Button);
 
@@ -21,20 +21,20 @@ public class Mover : MonoBehaviour
 
     [SerializeField]
     [Tooltip("How many meters per second to move when action is pressed")]
-    private float speed = 7, jumpForce=15f, maxjump=3;
+    private float speed = 7, jumpForce = 15f, maxjump = 3;
 
     private Rigidbody2D rb; 
 
     private void Awake()
     {
-        rb = GetComponent<Rigidbody2D>(); 
+        rb = GetComponent<Rigidbody2D>();
     }
 
     string currentGroundTag = "Red";
     string bottomTringle = "Red";
 
-    public bool CanMove(){
-       
+    public bool CanMove()
+    {
         return (bottomTringle == currentGroundTag || currentGroundTag == "Joker");
     }
     private void OnEnable()
@@ -54,15 +54,14 @@ public class Mover : MonoBehaviour
     }
     private void Update()
     {
-        if(CanMove()){
+        if (CanMove() )
+        {
             if (moveRight.IsPressed())
             {
-                
                 transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
             }
             if (moveLeft.IsPressed())
             {
-                
                 transform.position += new Vector3(-speed * Time.deltaTime, 0, 0);
             }
             if (moveDown.IsPressed())
@@ -71,36 +70,38 @@ public class Mover : MonoBehaviour
                 transform.position += new Vector3(0, -speed * Time.deltaTime, 0);
             }
         }
-        if (moveUp.IsPressed()&&jumpCounter<=maxjump)
-            {
-                rb.AddForce(new Vector2(0, speed * jumpForce));
-                jumpCounter++;
-                
-            }
+        if (moveUp.IsPressed() && jumpCounter <= maxjump)
+        {
+            rb.AddForce(new Vector2(0, speed * jumpForce));
+            jumpCounter++;
+        }
     }
-    
-    void OnCollisionEnter2D(Collision2D collision){
+    void OnCollisionEnter2D(Collision2D collision)
+    {
         Debug.Log("Collided with: " + collision.gameObject.tag);
         currentGroundTag = collision.gameObject.tag;
         currentAngle = transform.eulerAngles.z;
         jumpCounter=0;
         Debug.Log("Current angle: " + currentAngle);
-        if (currentAngle>startRedAngle && currentAngle<endRedAngle){
+        if (currentAngle > startRedAngle && currentAngle < endRedAngle)
+        {
             bottomTringle = "Red";
             Debug.Log("Bottom triangle is Red");
         }
-        else if (currentAngle>startGreenAngle && currentAngle<endGreenAngle){
+        else if (currentAngle > startGreenAngle && currentAngle < endGreenAngle)
+        {
             bottomTringle = "Green";
             Debug.Log("Bottom triangle is Green");
         }
-        else if (currentAngle>startBlueAngle && currentAngle<endBlueAngle){
+        else if (currentAngle > startBlueAngle && currentAngle < endBlueAngle)
+        {
             bottomTringle = "Blue";
             Debug.Log("Bottom triangle is Blue");
         }
-         Debug.Log($"Can move?: {(bottomTringle == currentGroundTag)||currentGroundTag == "Joker"} Because bottomTringle is {bottomTringle} and currentGroundTag is {currentGroundTag}");
+         Debug.Log($"Can move?: {(bottomTringle == currentGroundTag) || currentGroundTag == "Joker"} Because bottomTringle is {bottomTringle} and currentGroundTag is {currentGroundTag}");
     }
-    void OnCollisionStay2D(Collision2D collision){
-        
+    void OnCollisionStay2D(Collision2D collision)
+    {
         string currentGroundTag = collision.gameObject.tag;
     }
      
